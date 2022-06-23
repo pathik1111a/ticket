1c3c08930560343ed58694b04d0bb01667789c01
Pathik <75742550+pathik1111a@users.noreply.github.com>
2022-06-23 22:10:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  prefix: "$",
+  buttons: {
+    support: "https://discord.gg/Z7AZT7syJ9",
+    invite: "https://discord.gg/Z7AZT7syJ9",
+  },
+  mainembed: {
+    description: "To create a ticket, click the button that suits your request!",
+  },
+token: process.env.TOKEN
+};
